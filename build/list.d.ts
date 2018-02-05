1f927eb0ccada73c8d6473588e0f09c839e83600
Karoun Kasraie <contact@karoun.me>
2018-02-05 11:26:46 -0800

    Removing build/ directory in prep for PR

--- build/list.d.ts
+++
@@ -1,8 +0,0 @@
-import { StrongInputType, StrongInputOutputType, StrongOutputType } from './type';
-/**
- * Creates a strong list type where the inner type is whatever GraphQL strong
- * type is passed in.
- */
-export declare function createListType<TValue>(type: StrongInputOutputType<TValue>): StrongInputOutputType<Array<TValue>>;
-export declare function createListType<TValue>(type: StrongInputType<TValue>): StrongInputType<Array<TValue>>;
-export declare function createListType<TValue>(type: StrongOutputType<TValue>): StrongOutputType<Array<TValue>>;
--- build/nullable.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function createNullableType(type) {
-    return type.nullable();
-}
-exports.createNullableType = createNullableType;
